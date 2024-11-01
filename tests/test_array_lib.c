5a310779d0183c05f54df03c15398e8a8ded545e
hermyp <hermy@sakujit.su>
2024-11-02 03:48:31 +0500

    test_int64_zerosize_push

--- tests/test_array_lib.c
+++ tests/test_array_lib.c
@@ -8,14 +8,26 @@
 #include "../src/tests_lib/tests_fixture.h"
 
 #define INT32_ZEROSIZE_PUSH_CASE 0x12345678
+#define INT64_ZEROSIZE_PUSH_CASE 0x123456789ABCDEF0
 
 int test_array_int32_zerosize_push() {
     array_t *arr = array_new(sizeof(int32_t));
     if (!arr) return TEST_FAIL;
     int result = TEST_PASSED;
-    int val = INT32_ZEROSIZE_PUSH_CASE;
+    int32_t val = INT32_ZEROSIZE_PUSH_CASE;
     result &= array_push_back(arr, &val);
     result &= *(int32_t *)array_at(arr, 0) == INT32_ZEROSIZE_PUSH_CASE;
+    array_destroy(arr);
+    return result;
+}
+
+int test_array_int64_zerosize_push() {
+    array_t *arr = array_new(sizeof(int64_t));
+    if (!arr) return TEST_FAIL;
+    int result = TEST_PASSED;
+    int64_t val = INT64_ZEROSIZE_PUSH_CASE;
+    result &= array_push_back(arr, &val);
+    result &= *(int64_t *)array_at(arr, 0) == INT64_ZEROSIZE_PUSH_CASE;
     array_destroy(arr);
     return result;
 }
@@ -23,8 +35,9 @@ int test_array_int32_zerosize_push() {
 int main() {
     Test *test =
         init_test("test_array_lib",
-                  " These test cases perform unit testing\n of the \"array_lib\" library functionality", 1,
-                  (TestCase)test_array_int32_zerosize_push, "test_array_int32_zerosize_push");
+                  " These test cases perform unit testing\n of the \"array_lib\" library functionality", 2,
+                  (TestCase)test_array_int32_zerosize_push, "test_array_int32_zerosize_push",
+                  (TestCase)test_array_int64_zerosize_push, "test_array_int64_zerosize_push");
     if (test) {
         run_test(test);
         destroy_test(test);
